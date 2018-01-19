1aeed172938d863ca2b0399c241d31c03f547f47
Morven Lewis-Everley <morven@i-lateral.com>
2018-01-19 12:06:00 +0000

    Add check to ensure item has a download link

--- code/extensions/DownloadableOrderItemExtension.php
+++ code/extensions/DownloadableOrderItemExtension.php
@@ -6,8 +6,9 @@ class DownloadableOrderItemExtension extends DataExtension
     public function getDownloadLink()
     {
         $order = Order::get()->byID($this->owner->ParentID);
+        $match = $this->owner->match();
 
-        if ($match = $this->owner->match()) {
+        if ($match && method_exists($match, "getDownloadLink")) {
             return $match->getDownloadLink().
                 '?o='.$order->ID.
                 '&k='.$order->AccessKey;
